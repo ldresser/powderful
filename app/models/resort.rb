de08fbe5c220bb7009f5951bca651f0ad131560b
Matt Boys <matt.boys@gmail.com>
2012-12-02 18:18:41 -0500

    some more cleanup

--- app/models/resort.rb
+++ app/models/resort.rb
@@ -1,5 +1,5 @@
 class Resort < ActiveRecord::Base
-	extend Geocoder::Model::ActiveRecord
+	
   attr_accessible :address, :area, :city, :elevation_base, :elevation_peak, :lifts, :longest_trail, :name, :pr_advanced, :pr_beginner, :pr_expert, :pr_intermediate, :runs, :state, :trail_map, :url, :vertical_drop, :zip, :latitude, :longitude, :snowcountryid
 
 
