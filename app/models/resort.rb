34f7e036c32c8cd462763ab5c9532fa77a32f081
Matt Boys <matt.boys@gmail.com>
2012-12-02 18:13:18 -0500

    some cleanup

--- app/models/resort.rb
+++ app/models/resort.rb
@@ -3,15 +3,7 @@ class Resort < ActiveRecord::Base
   attr_accessible :address, :area, :city, :elevation_base, :elevation_peak, :lifts, :longest_trail, :name, :pr_advanced, :pr_beginner, :pr_expert, :pr_intermediate, :runs, :state, :trail_map, :url, :vertical_drop, :zip, :latitude, :longitude, :snowcountryid
 
 
-	geocoded_by :gAddress
-	# "#{:address}, #{:city}, #{:state} #{:zip}"
-	after_validation :geocode
 
-	geocoded_by :gAddress
-
-def gAddress
-  [address, city, state, zip].compact.join(', ')
-end
 
 
 end
